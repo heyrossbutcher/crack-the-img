9518ab7b69810be575d02a399e4a698f37156298
heyrossbutcher <heyrossbutcher@gmail.com>
2015-02-25 00:03:04 -0500

    Fixed the absolute float problem

--- script/script.js
+++ script/script.js
@@ -4,16 +4,12 @@ var app = {};
 
 app.dup = function(){
 // alert('this is being called');
-	app.classHolder = '.box';
-	app.classCounter = 0;
 
-	$(app.classHolder).on('click', function(){
-		app.classCounter ++;
-		console.log(app.classCounter + '!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+	$('.box').on('click', function(){
 		//THIS GETS THE POSITION OF THE BOX BEING CLICKED
-		app.pos = $(app.classHolder).position();
-		app.wid = $(app.classHolder).width();
-		app.hgt = $(app.classHolder).height();
+		app.pos = $(this).position();
+		app.wid = $(this).width();
+		app.hgt = $(this).height();
 		app.xpos = app.pos.top;
 		app.ypos = app.pos.left;
 		app.xoff = app.wid/2;
@@ -29,8 +25,8 @@ app.dup = function(){
 			bottomLeft = {put1 : app.yoff, put2 : app.xpos},
 			bottomRight = {put1 : app.yoff, put2 : app.xoff},
 		]
-		
 		$(this).remove();//THIS HIDES WHAT'S CLICKED
+
 		// app.classHolder = app.classHolder + app.classCounter;
 			//THS WILL CREATE THE NEW QUADRANTS
 			app.colorSelect = [
@@ -38,11 +34,9 @@ app.dup = function(){
 			]
 			for (var i = 0; i < 4; i++) {
 				app.bp = app.boxPlace[i];
-				var div = $('<div>').addClass(app.classHolder + app.classCounter).css('top', app.bp.put1).css('left', app.bp.put2).css('width', '50%').css('height', '50%').css('background', app.colorSelect[i]);
+				var div = $('<div>').addClass('box').css('top', app.bp.put1).css('left', app.bp.put2).css('width', '50%').css('height', '50%').css('background', app.colorSelect[i]);
 				div.appendTo('.wrapper');
 			};
-				// app.classHolder = app.classHolder + app.classCounter;
-				console.log(app.classHolder + '??????????????????????')
 	});
 }
 
