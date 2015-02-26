33f659e3789132cb8056980c3c132753b58d8bfb
heyrossbutcher <heyrossbutcher@gmail.com>
2015-02-26 00:17:32 -0500

    Solved the cascading effect on multiple images

--- script/script.js
+++ script/script.js
@@ -1,38 +1,54 @@
 var app = {};
 
 
-
+app.click_counter = 0;
+//
 app.dup = function(){
 // alert('this is being called');
 
 	$('img').on('click', function(){
+		app.click_counter++;		//
 		//THIS GETS THE POSITION OF THE BOX BEING CLICKED
 		app.pos = $(this).position();
 		app.wid = $(this).width();
 		app.hgt = $(this).height();
 		app.xpos = app.pos.top;
 		app.ypos = app.pos.left;
-		//
-		//CHECKING THE LOCATION
-		console.log('X position is: ' + app.xpos + '& Y position is: ' + app.ypos);
-		console.log('X Offset is: ' + app.xoff + '& Y Offset is: ' + app.yoff);
 		//THIS REMOVES WHAT'S CLICKED
 		$(this).addClass('hider');
-		console.log('Half the width is: ' + app.xoff + '   Half the length is: ' + app.yoff);
 		//THIS STORES THE SRC OF THE IMG CLICKED
 		app.grabImg = $(this).attr('src');
+		//CREATES THE IMAGE REPLACEMNT DIV
+		app.box_holder = 'box' + app.click_counter;
+		console.log(app.box_holder + ' This is the box holder');
+		var div = $('<div>').addClass(app.box_holder).css('top', app.xpos).css('left', app.ypos).css('width', app.wid).css('height', app.hgt);
+		//
+		div.appendTo($(this).parent());
+		// //
+		console.log('X position is: ' + app.xpos + '& Y position is: ' + app.ypos);
+		console.log('The width is: ' + app.wid + '   The length is: ' + app.hgt);
 		console.log('This is the image src: ' + app.grabImg);
-		//THS WILL CREATE THE NEW SPLINTER QUADRANTS
+
+
+		//
+		//ADD TO THE DOM
+		//CREATE THE QUADRANTS AND LOAD IN IMG
 		for (var i = 0; i < 6; i++) {
-			// app.bp = app.boxPlace[1];
-			app.counter = i + 1;
-			//CREATE THE QUADRANTS AND LOAD IN IMG
-			// var div = $(',div').add
-			var div_img = $('<div>').addClass('crack' + app.counter).css('top', app.xpos).css('left', app.ypos).css('width', app.wid).css('height', app.hgt);	
+			app.counter = i+1;
+			console.log(app.counter);
+
+			app.crack_holder = 'crack' + app.counter;
+			app.img_crack_holder = '.' + app.box_holder + ' ' + '.' +  app.crack_holder;
+			console.log(app.img_crack_holder + '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+
+			var div_img = $('<div>').addClass(app.crack_holder).css('top', app.xpos).css('left', app.ypos).css('width', app.wid).css('height', app.hgt);	
+			div_img.appendTo(div);
+
 			var img = $('<img>').attr('src', app.grabImg);
-			//ADD TO THE DOM
-			div.appendTo('.wrapper');
-			img.appendTo('.crack' + app.counter);
+
+			// div_img.appendTo('.box' + app.click_counter);
+		// 	// console.log(app.counter);
+			img.appendTo(app.img_crack_holder);
 		};
 	});
 }
