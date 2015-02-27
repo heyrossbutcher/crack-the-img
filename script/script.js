8761cb4f9c3d1f849854698db6d58cdf90c3b138
heyrossbutcher <heyrossbutcher@gmail.com>
2015-02-27 15:06:00 -0500

    Refactored name space out

--- script/script.js
+++ script/script.js
@@ -1,39 +1,35 @@
-var app = {};
-//
-app.click_counter = 0;
-//
-app.dup = function(opacBoo, snd){
-// alert('this is being called');
-
+crackerJack = function(opacBoo, snd){
+	click_counter = 0;
+	//
 	$('img').on('click', function(){
-		app.click_counter++;		//
+		click_counter++;		//
 		//THIS GETS THE POSITION OF THE BOX BEING CLICKED
-		app.pos = $(this).position();
-		app.wid = $(this).width();
-		app.hgt = $(this).height();
-		app.xpos = app.pos.top;
-		app.ypos = app.pos.left;
+		pos = $(this).position();
+		wid = $(this).width();
+		hgt = $(this).height();
+		xpos = pos.top;
+		ypos = pos.left;
 		//THIS REMOVES WHAT'S CLICKED
 		$(this).addClass('hider');
 		//THIS STORES THE SRC OF THE IMG CLICKED
-		app.grabImg = $(this).attr('src');
+		grabImg = $(this).attr('src');
 		//CREATES THE IMAGE REPLACEMNT DIV
-		app.box_holder = 'box' + app.click_counter;
-		console.log(app.box_holder + ' This is the box holder');
-		var div = $('<div>').addClass(app.box_holder).css('top', app.xpos).css('left', app.ypos).css('width', app.wid).css('height', app.hgt);
+		box_holder = 'box' + click_counter;
+		console.log(box_holder + ' This is the box holder');
+		var div = $('<div>').addClass(box_holder).css('top', xpos).css('left', ypos).css('width', wid).css('height', hgt);
 		//
 		div.appendTo($(this).parent());
 		// //
-		console.log('X position is: ' + app.xpos + '& Y position is: ' + app.ypos);
-		console.log('The width is: ' + app.wid + '   The length is: ' + app.hgt);
-		console.log('This is the image src: ' + app.grabImg);
+		console.log('X position is: ' + xpos + '& Y position is: ' + ypos);
+		console.log('The width is: ' + wid + '   The length is: ' + hgt);
+		console.log('This is the image src: ' + grabImg);
 
 		//ADD TO THE DOM
 		//CREATE THE QUADRANTS AND LOAD IN IMG
-		app.opacSwitcher = opacBoo;
-		app.crackSnd	= snd;
+		opacSwitcher = opacBoo;
+		crackSnd	= snd;
 		//
-		if(app.crackSnd){
+		if(crackSnd){
 			var audioElement = document.createElement('audio');
 	        audioElement.setAttribute('src', 'sdfx/crack.mp3');
 	        audioElement.setAttribute('autoplay', 'autoplay');
@@ -41,31 +37,31 @@ app.dup = function(opacBoo, snd){
 		}
         //
 		for (var i = 0; i < 20; i++) {
-			app.counter = i+1;
+			counter = i+1;
 			//
-			app.opacArray = [0.88, 0.91, 0.94, 0.97, 1];
-			app.opacLength = app.opacArray.length;
-			app.opacNum = Math.floor(Math.random() * app.opacLength);
-			app.opac = app.opacArray[app.opacNum];
-			// $(this).css('opacity', app.opac);
-			console.log('This is the opacity: ' + app.opac);
-			app.crack_holder = 'crack' + app.counter;
-			app.img_crack_holder = '.' + app.box_holder + ' ' + '.' +  app.crack_holder;
+			opacArray = [0.88, 0.91, 0.94, 0.97, 1];
+			opacLength = opacArray.length;
+			opacNum = Math.floor(Math.random() * opacLength);
+			opac = opacArray[opacNum];
+			// $(this).css('opacity', opac);
+			console.log('This is the opacity: ' + opac);
+			crack_holder = 'crack' + counter;
+			img_crack_holder = '.' + box_holder + ' ' + '.' +  crack_holder;
 			//
-			var div_img = $('<div>').addClass(app.crack_holder).css('top', app.xpos).css('left', app.ypos).css('width', app.wid).css('height', app.hgt);	
+			var div_img = $('<div>').addClass(crack_holder).css('top', xpos).css('left', ypos).css('width', wid).css('height', hgt);	
 			div_img.appendTo(div);
 			//
-			var img = $('<img>').attr('src', app.grabImg);
+			var img = $('<img>').attr('src', grabImg);
 			//
-			img.appendTo(app.img_crack_holder);
-			if (app.opacSwitcher){
-				$(app.img_crack_holder).delay(2500).fadeTo(1, app.opac);
+			img.appendTo(img_crack_holder);
+			if (opacSwitcher){
+				$(img_crack_holder).delay(2500).fadeTo(1, opac);
 			}
 		};
 	});
 }
 
 $(function() {
-	app.dup(true, false);
+	crackerJack(true, true);
 	
 });
